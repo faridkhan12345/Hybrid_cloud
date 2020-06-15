2f8444259f31761fa20daf3cce19342c500c59ec
faridkhan12345 <66347167+faridkhan12345@users.noreply.github.com>
2020-06-15 15:50:52 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+echo ' <marquee> WECOME TO MY CLOUD WORLD </marquee> ';
+<br />
+
+
+
+<pre>
+<?php
+echo' 
+<html>
+<center> <img src="https://raw.githubusercontent.com/himanshuthapliyal/RIGHT_EDU/master/NmDIPyO_.jpg"  width="300" height="400">  </center>
+</html>
+';
+
+
+
+?>
+</pre>
